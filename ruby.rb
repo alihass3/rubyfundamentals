93511b04675c57479402a437301a5f7e1d56e4a3
User <user@Users-MacBook-Air.local>
2018-09-25 00:06:58 -0400

    added loop method

--- ruby.rb
+++ ruby.rb
@@ -5,3 +5,28 @@ puts "show up on github"
 puts "PUll thingy"
 
 
+name= :Hassan
+puts "My name is #{name}"
+
+puts "What is your name?"
+user_name = gets
+puts "Hello, #{user_name}"
+
+distance=0
+while true
+  puts "Would you like to walk or run?"
+  user_response=(gets.chomp).to_s
+  if (user_response=="walk")
+    distance+=1
+    puts "Distance from home is #{distance}km."
+  elsif (user_response=="run")
+   distance+=5
+   puts "Distance from home is #{distance}km."
+  elsif (user_response=="go home")
+   break
+  else
+    puts "Invalid input"
+  end
+
+
+end
